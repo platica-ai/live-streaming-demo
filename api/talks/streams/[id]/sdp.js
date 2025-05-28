6ce58f9015ade0ce274056a81850f7234148e17d
platica-ai <gabriel@scplatica.com>
2025-05-28 09:40:26 +0300

    Update sdp.js

--- api/talks/streams/[id]/sdp.js
+++ api/talks/streams/[id]/sdp.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { id } = req.query;
 
-  const response = await fetch(`https://api.d-id.com/talks/streams/${id}/ice`, {
+  const response = await fetch(`https://api.d-id.com/talks/streams/${id}/sdp`, {
     method: 'POST',
     headers: {
       'Authorization': `Basic ${process.env.DID_API_KEY}`,
