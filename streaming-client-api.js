1e16b9fc6960f85df73879b5bd55f965b9cf309d
platica-ai <gabriel@scplatica.com>
2025-05-28 08:45:47 +0300

    Update streaming-client-api.js

--- streaming-client-api.js
+++ streaming-client-api.js
@@ -16,7 +16,6 @@ let sessionId;
 
 const idleVideoElement = document.getElementById('idle-video-element');
 const streamVideoElement = document.getElementById('stream-video-element');
-idleVideoElement.src = '/luna_idle.mp4';  // Ensure you have this file in your public directory
 
 const presenterInputByService = {
   talks: {
@@ -24,11 +23,74 @@ const presenterInputByService = {
   },
 };
 
+async function fetchWithRetries(url, options, retries = 1) {
+  const maxRetryCount = 3;
+  const maxDelaySec = 4;
+
+  try {
+    return await fetch(url, options);
+  } catch (err) {
+    if (retries <= maxRetryCount) {
+      const delay = Math.min(Math.pow(2, retries), maxDelaySec) * 1000;
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      return fetchWithRetries(url, options, retries + 1);
+    } else {
+      throw new Error(`Max retries exceeded. error: ${err}`);
+    }
+  }
+}
+
+async function createPeerConnection(offer, iceServers) {
+  peerConnection = new RTCPeerConnection({ iceServers });
+
+  peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      fetch(`${DID_API.url}/talks/streams/${streamId}/ice`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${DID_API.key}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          candidate: event.candidate.candidate,
+          sdpMid: event.candidate.sdpMid,
+          sdpMLineIndex: event.candidate.sdpMLineIndex,
+          session_id: sessionId,
+        }),
+      });
+    }
+  };
+
+  peerConnection.ontrack = (event) => {
+    streamVideoElement.srcObject = event.streams[0];
+    streamVideoElement.play();
+  };
+
+  await peerConnection.setRemoteDescription(offer);
+  const sessionClientAnswer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(sessionClientAnswer);
+
+  return sessionClientAnswer;
+}
+
+function stopAllStreams() {
+  if (streamVideoElement.srcObject) {
+    streamVideoElement.srcObject.getTracks().forEach((track) => track.stop());
+    streamVideoElement.srcObject = null;
+  }
+}
+
+function closePC() {
+  if (!peerConnection) return;
+  peerConnection.close();
+  peerConnection = null;
+}
+
 document.getElementById('connect-button').onclick = async () => {
   stopAllStreams();
   closePC();
 
-  const sessionResponse = await fetch(`${DID_API.url}/stream`, {
+  const sessionResponse = await fetchWithRetries(`${DID_API.url}/stream`, {
     method: 'POST',
     headers: {
       Authorization: `Basic ${DID_API.key}`,
@@ -37,71 +99,30 @@ document.getElementById('connect-button').onclick = async () => {
     body: JSON.stringify({ ...presenterInputByService[DID_API.service], stream_warmup: true }),
   });
 
-  const { id, offer, ice_servers, session_id } = await sessionResponse.json();
-  streamId = id;
-  sessionId = session_id;
+  const data = await sessionResponse.json();
+  streamId = data.id;
+  sessionId = data.session_id;
 
   window.streamId = streamId;
   window.sessionId = sessionId;
 
-  const peerConnectionConfig = { iceServers: ice_servers };
-  peerConnection = new RTCPeerConnection(peerConnectionConfig);
+  const sessionClientAnswer = await createPeerConnection(data.offer, data.ice_servers);
 
-  peerConnection.ontrack = (event) => {
-    streamVideoElement.srcObject = event.streams[0];
-  };
-
-  peerConnection.onicecandidate = ({ candidate }) => {
-    if (candidate) {
-      fetch(`${DID_API.url}/ice`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic ${DID_API.key}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          candidate: candidate.candidate,
-          sdpMid: candidate.sdpMid,
-          sdpMLineIndex: candidate.sdpMLineIndex,
-          session_id: sessionId,
-          stream_id: streamId
-        }),
-      });
-    }
-  };
-
-  await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-  const answer = await peerConnection.createAnswer();
-  await peerConnection.setLocalDescription(answer);
-
-  await fetch(`${DID_API.url}/sdp`, {
+  await fetch(`${DID_API.url}/talks/streams/${streamId}/sdp`, {
     method: 'POST',
     headers: {
       Authorization: `Basic ${DID_API.key}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      answer,
-      session_id: sessionId,
-      stream_id: streamId
-    }),
+    body: JSON.stringify({ answer: sessionClientAnswer, session_id: sessionId }),
   });
 };
 
-function stopAllStreams() {
-  if (streamVideoElement.srcObject) {
-    streamVideoElement.srcObject.getTracks().forEach(track => track.stop());
-    streamVideoElement.srcObject = null;
-  }
-}
-
-function closePC() {
-  if (peerConnection) {
-    peerConnection.close();
-    peerConnection = null;
-  }
-}
-
 window.onload = async () => {
   const res = await fetch('/api/env');
-  const data = await res.json
+  const data = await res.json();
+  DID_API.key = data.DID_API_KEY;
+
+  idleVideoElement.src = 'luna_idle.mp4';
+  idleVideoElement.play();
+};
